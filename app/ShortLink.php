675eeea545b74018faf42f6ec91347aa90a54ee3
Nilov Mikhail <OTV5125@gmail.com>
2020-05-10 17:56:22 +0300

    ADD ShortLinkModel

--- app/ShortLink.php
+++ app/ShortLink.php
@@ -3,10 +3,30 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Services\Service;
 
 class ShortLink extends Model
 {
     protected $table = 'short_link';
     public $timestamps = false;
-    protected $guarded = array('user_id', 'url', 'title', 'description', 'image', 'code');
+    protected $fillable = array('user_id', 'url', 'title', 'description', 'image', 'code');
+
+    public function addRecord($param){
+        $result = ShortLink::select('id', 'code')->orderBy('id', 'desc')->first();
+
+        if(is_null($result)){
+            $lastCode = null;
+            $id = 1;
+        }else{
+            $lastCode = $result->code;
+            $id = $result->id;
+        }
+        $code = Service::generateCode($lastCode, -1);
+        $image = $param['image'];
+        $param['image'] = $id.'.'.$image->getClientOriginalExtension();
+        $param['code'] = $code;
+        self::create($param);
+        $image->storeAs('public/images', $param['image']);
+        return $code;
+    }
 }
